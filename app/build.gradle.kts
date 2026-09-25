6f1832e0983873c66535bb4549ad067a5ff162fd
DDwsgood <ddwsgood@users.noreply.github.com>
2026-09-26 01:11:21 +0800

    chore: bump to 2.5.9 (vc181)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,8 +22,8 @@ android {
         applicationId = "excp.rikkahub"
         minSdk = 26
         targetSdk = 37
-        versionCode = 180
-        versionName = "2.5.8"
+        versionCode = 181
+        versionName = "2.5.9"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
